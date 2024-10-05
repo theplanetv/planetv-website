d9d12eb716dec901929557d7d1581901f1569391
goatastronaut0212 <goatastronaut0212@outlook.com>
2024-10-05 21:13:13 +0700

    Add tailwindcss to web-nuxt

---
+++ web-nuxt/tailwind.config.ts
@@ -0,0 +1,18 @@
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./app.vue",
+    "./error.vue",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+export default config;
