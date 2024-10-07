f660db4c853ba47a74e430a5e9a96350d775a661
goatastronaut0212 <goatastronaut0212@outlook.com>
2024-10-07 17:51:00 +0700

    Add blog_tag functions and test values database-postgresql

---
+++ database-postgresql/src/insert-test.sql
@@ -0,0 +1,4 @@
+-- blog_tag
+INSERT INTO blog_tag VALUES ('de36d383-cf7c-43a0-98f1-73e9026bdd41', 'tag 1');
+INSERT INTO blog_tag VALUES ('38083c99-70d4-4027-83e4-5601d8cfc59c', 'tag 2');
+INSERT INTO blog_tag VALUES ('be11c4ce-3b51-40d8-bb42-f7923cddaa04', 'tag 11');
