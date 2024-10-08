f363325c34885219a1fe07735435764e8afc13a9
goatastronaut0212 <goatastronaut0212@outlook.com>
2024-10-08 10:58:35 +0700

    Add test script

---
+++ scripts/test.bash
@@ -0,0 +1,66 @@
+export $(grep -v '^#' .env | xargs)
+
+PROJECT_API_CONTAINER=$(eval echo $PROJECT_API_CONTAINER)
+PROJECT_DATABASE_CONTAINER=$(eval echo $PROJECT_DATABASE_CONTAINER)
+
+test_wait_postgresql() {
+	# Maximum number of attempts to check PostgreSQL readiness
+	max_attempts=5
+
+	# Counter for tracking the number of attempts
+	attempt_count=0
+
+	# Check PostgreSQL readiness with a loop
+	while [ $attempt_count -lt $max_attempts ]; do
+		# Try to check PostgreSQL readiness with a 5-second timeout for each attempt
+		if timeout 5 docker exec ${PROJECT_DATABASE_CONTAINER} pg_isready; then
+			echo "PostgreSQL is ready"
+			break
+		else
+			echo "Waiting for PostgreSQL to become ready... attempt $((attempt_count + 1))"
+		fi
+		attempt_count=$((attempt_count + 1))
+		sleep 2
+	done
+
+	# If PostgreSQL is not ready after max_attempts, exit with an error
+	if [ $attempt_count -ge $max_attempts ]; then
+		echo "PostgreSQL is not ready after $max_attempts attempts. Exiting..."
+		exit 1
+	fi
+
+	# Run the test if PostgreSQL is ready
+	echo "PostgreSQL is ready after $attempt_count attempts. Running the test..."
+
+	case $1 in
+		"service-blogtag" )
+		docker exec ${PROJECT_API_CONTAINER} go test -v \
+			/api-chi/cmd/services/blogtag.go /api-chi/cmd/services/blogtag_test.go
+		;;
+	esac
+}
+
+# Print list of options
+print_list() {
+	echo "Pass wrong arguments! Here is list of arguments for docker test script"
+	echo -e "\ttest : test all unit test"
+}
+
+# Main script
+if [ $# -eq 1 ]; then
+	case "$1" in
+		"test-api-service-blogtag" )
+			test_wait_postgresql service-blogtag ;;
+
+		"test-coverage" )
+			docker exec ${PROJECT_API_CONTAINER} go test -coverprofile=coverage.out ./...
+			docker exec ${PROJECT_API_CONTAINER} go tool cover -html=coverage.out -o ./coverage.html
+			docker cp ${PROJECT_API_CONTAINER}:/api-chi/coverage.html ./coverage.html
+			;;
+
+		* )
+			print_list ;;
+	esac
+else
+	print_list
+fi
