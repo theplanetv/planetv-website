5c842db8a995a7a3506983ce11b34c88ec8e61d9
goatastronaut0212 <goatastronaut0212@outlook.com>
2024-10-23 08:11:38 +0700

    Update api-chi with web host and cors config

--- api-chi/cmd/config/web.go
+++ api-chi/cmd/config/web.go
@@ -4,9 +4,11 @@ import "os"
 
 var (
 	// Web config
+	WEB_HOST string
 	WEB_PORT string
 )
 
 func LoadWebConfig() {
+	WEB_HOST = os.Getenv("WEB_HOST")
 	WEB_PORT = os.Getenv("WEB_PORT")
 }
--- api-chi/cmd/models/blogtag.go
+++ api-chi/cmd/models/blogtag.go
@@ -1,6 +1,6 @@
 package models
 
 type BlogTag struct {
-	Id   string
-	Name string
+	Id   string `json:"id"`
+	Name string `json:"name"`
 }
