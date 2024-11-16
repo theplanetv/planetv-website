03548d72eeef5d9275783c1c6ae9128a20db3b41
goatastronaut0212 <goatastronaut0212@outlook.com>
2024-11-16 15:17:07 +0700

    web-next-ts: add prettier

---
+++ web-next-ts/prettier.config.ts
@@ -0,0 +1,14 @@
+import { Config } from "prettier";
+
+const config: Config = {
+  tabWidth: 2,
+  semi: false,
+  singleQuote: false,
+  trailingComma: "es5",
+  bracketSpacing: true,
+  arrowParens: "always",
+  endOfLine: "lf",
+  printWidth: 80,
+};
+
+export default config;
